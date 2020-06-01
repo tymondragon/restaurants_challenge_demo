f5cb3471480c9711918cb18cffaa80868c34b23e
Ty Mondragon <tymondragon@gmail.com>
2020-06-01 12:50:28 -0600

    add pg and move knexfile to root

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,12 @@
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+require('dotenv').config({
+  path: require('path').join(__dirname, envFile)
+});
+
+const migrations = {
+  directory: './server/migrations'
+}
+
 module.exports = {
   development: {
     client: 'pg',
